f6737e0ffdf49785c3a620d1a2777f372626ee61
raimondr <raimondr@raimondr-mac.ria.ee>
2017-05-10 10:12:15 +0300

    .

--- src/main/resources/static/js/TableSorter.js
+++ src/main/resources/static/js/TableSorter.js
@@ -1,24 +1,26 @@
-$('.hideAppr').on('click', function () {
+$(document).ready(function() {
+	$('.hideAppr').on('click', function () {
   		var $rowsAppr = $('#info-systems-table tbody tr').filter(function () {
             return $.trim($(this).find('td').eq(6).text()) !== ""}).toggle();
   		
-	//for normal buttons
-	//$(this).text(function(i, v){
-    // return v === 'Näita kooskõlastamist vajavaid' ? 'Näita Kõiki' : 'Näita kooskõlastamist vajavaid'})
-});
+  		//for normal buttons
+  		//$(this).text(function(i, v){
+          // return v === 'Näita kooskõlastamist vajavaid' ? 'Näita Kõiki' : 'Näita kooskõlastamist vajavaid'})
+    });
 
-$('.hideEmptyAppr').on('click', function () {
-	var $rowsEmptyAppr = $('#info-systems-table tbody tr').filter(function () {
-         return $.trim($(this).find('td').eq(6).text()) === ""}).toggle();
+  	$('.hideEmptyAppr').on('click', function () {
+  		var $rowsEmptyAppr = $('#info-systems-table tbody tr').filter(function () {
+            return $.trim($(this).find('td').eq(6).text()) === ""}).toggle();
   		
-	//for normal buttons:
-	//$(this).text(function(i, v){
-    //return v === 'Näita ainult kooskõlastatud' ? 'Näita Kõiki' : 'Näita ainult kooskõlastatud'})
-});
-$('.findOwner').on('click', function () {
-	var $rowsOwner = $('#info-systems-table tbody tr').filter(function () {
-       return $.trim($(this).find('td').eq(0).text()) !== document.getElementById("owner").value}).toggle();
+  		//for normal buttons:
+  		//$(this).text(function(i, v){
+           //return v === 'Näita ainult kooskõlastatud' ? 'Näita Kõiki' : 'Näita ainult kooskõlastatud'})
+    });
+  	$('.findOwner').on('click', function () {
+  		var $rowsOwner = $('#info-systems-table tbody tr').filter(function () {
+            return $.trim($(this).find('td').eq(0).text()) !== document.getElementById("owner").value}).toggle();
   		
-	$(this).text(function(i, v){
-   		return v === 'Otsi omaniku' ? 'Näita Kõiki' : 'Otsi omaniku'})
+  		$(this).text(function(i, v){
+       		return v === 'Otsi omaniku' ? 'Näita Kõiki' : 'Otsi omaniku'})
+    });
 });
