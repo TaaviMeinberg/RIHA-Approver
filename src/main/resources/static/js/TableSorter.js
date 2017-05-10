ed65d300f17dfce7fa6b94b239527d319ccd5ded
raimondr <raimondr@raimondr-mac.ria.ee>
2017-05-10 14:56:55 +0300

    idk

--- src/main/resources/static/js/TableSorter.js
+++ src/main/resources/static/js/TableSorter.js
@@ -15,28 +15,28 @@ $(document).ready(function() {
   		//$(this).text(function(i, v){
            //return v === 'Näita ainult kooskõlastatud' ? 'Näita Kõiki' : 'Näita ainult kooskõlastatud'})
     });
-//  	$('.findOwner').on('click', function () {
-//  		var $rowsOwner = $('#info-systems-table tbody tr').filter(function () {
-//            return $.trim($(this).find('td').eq(0).text()) !== document.getElementById("owner").value}).toggle();
-//  		
+  	$('.owner').on('keyup', function () {
+  		var $rowsOwner = $('#info-systems-table tbody tr').filter(function () {
+            return $.trim($(this).find('td').eq(0).text()) !== document.getElementById("owner").value});
+  		
 //  		$(this).text(function(i, v){
 //       		return v === 'Otsi omaniku' ? 'Näita Kõiki' : 'Otsi omaniku'})
-//    });
-  	$('.findOwner').on('keyup', function () {
-  	  var input, filter, table, tr, td, i;
-  	  input = document.getElementById("owner");
-  	  filter = input.value.toUpperCase();
-  	  table = document.getElementById("info-systems-table");
-  	  tr = table.getElementsByTagName("tr");
-  	  for (i = 0; i < tr.length; i++) {
-  	    td = tr[i].getElementsByTagName("td")[0];
-  	    if (td) {
-  	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-  	        tr[i].style.display = "";
-  	      } else {
-  	        tr[i].style.display = "none";
-  	      }
-  	    }       
-  	  }
-  	}
+    });
+//  	$('.findOwner').on('keyup', function () {
+//  	  var input, filter, table, tr, td, i;
+//  	  input = document.getElementById("owner");
+//  	  filter = input.value.toUpperCase();
+//  	  table = document.getElementById("info-systems-table");
+//  	  tr = table.getElementsByTagName("tr");
+//  	  for (i = 0; i < tr.length; i++) {
+//  	    td = tr[i].getElementsByTagName("td")[0];
+//  	    if (td) {
+//  	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//  	        tr[i].style.display = "";
+//  	      } else {
+//  	        tr[i].style.display = "none";
+//  	      }
+//  	    }       
+//  	  }
+//  	}
 });
