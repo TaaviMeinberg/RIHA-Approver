43d908cf928934a134455060b2b995f0748ff739
raimondr <raimondr@raimondr-mac.ria.ee>
2017-05-10 12:20:23 +0300

    tried a different searh bar function

--- src/main/resources/static/js/TableSorter.js
+++ src/main/resources/static/js/TableSorter.js
@@ -15,11 +15,28 @@ $(document).ready(function() {
   		//$(this).text(function(i, v){
            //return v === 'Näita ainult kooskõlastatud' ? 'Näita Kõiki' : 'Näita ainult kooskõlastatud'})
     });
-  	$('.findOwner').on('click', function () {
-  		var $rowsOwner = $('#info-systems-table tbody tr').filter(function () {
-            return $.trim($(this).find('td').eq(0).text()) !== document.getElementById("owner").value}).toggle();
-  		
-  		$(this).text(function(i, v){
-       		return v === 'Otsi omaniku' ? 'Näita Kõiki' : 'Otsi omaniku'})
-    });
+//  	$('.findOwner').on('click', function () {
+//  		var $rowsOwner = $('#info-systems-table tbody tr').filter(function () {
+//            return $.trim($(this).find('td').eq(0).text()) !== document.getElementById("owner").value}).toggle();
+//  		
+//  		$(this).text(function(i, v){
+//       		return v === 'Otsi omaniku' ? 'Näita Kõiki' : 'Otsi omaniku'})
+//    });
+  	function searchFun2() {
+  	  var input, filter, table, tr, td, i;
+  	  input = document.getElementById("searchOwner");
+  	  filter = input.value.toUpperCase();
+  	  table = document.getElementById("#info-systems-table");
+  	  tr = table.getElementsByTagName("tr");
+  	  for (i = 0; i < tr.length; i++) {
+  	    td = tr[i].getElementsByTagName("td")[0];
+  	    if (td) {
+  	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+  	        tr[i].style.display = "";
+  	      } else {
+  	        tr[i].style.display = "none";
+  	      }
+  	    }       
+  	  }
+  	}
 });
