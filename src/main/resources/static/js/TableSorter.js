317b6866e2c2c57b330560d36143972a20b7ede9
raimondr <raimondr@raimondr-mac.ria.ee>
2017-05-18 15:20:52 +0300

    reverted the changes

--- src/main/resources/static/js/TableSorter.js
+++ src/main/resources/static/js/TableSorter.js
@@ -18,27 +18,9 @@ $(document).ready(function() {
 
   	$('.findOwner').on('click', function () {
   		var $rowsOwner = $('#info-systems-table tbody tr').filter(function () {
-            return $.trim($(this).find('td:not(:contains(("#ownerS").value))').eq(0).text())}).toggle();   //!== document.getElementById("owner").value}).toggle();
-  		
+  			return $.trim($(this).find('td').eq(0).text() !== document.getElementById("owner").value}).toggle();
+  		//return $.trim($(this).find('td:not(:contains(("#ownerS").value))').eq(0).text())}).toggle();   //
   		$(this).text(function(i, v){
        		return v === 'Otsi omaniku' ? 'Näita Kõiki' : 'Otsi omaniku'})
-  	});
-  	
-//  	$('.findOwner').on('keyup', function () {
-//  	  var input, filter, table, tr, td, i;
-//  	  input = document.getElementById("owner");
-//  	  filter = input.value.toUpperCase();
-//  	  table = document.getElementById("info-systems-table");
-//  	  tr = table.getElementsByTagName("tr");
-//  	  for (i = 0; i < tr.length; i++) {
-//  	    td = tr[i].getElementsByTagName("td")[0];
-//  	    if (td) {
-//  	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//  	        tr[i].style.display = "";
-//  	      } else {
-//  	        tr[i].style.display = "none";
-//  	      }
-//  	    }       
-//  	  }
-//  	}
+  	}));
 });
