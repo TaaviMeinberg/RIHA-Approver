d40c27b5ada319b72518f672c03f5a70b60b1f75
raimondr <raimondr@raimondr-mac.ria.ee>
2017-06-07 10:15:31 +0300

    Added some comments.

--- src/main/resources/static/js/TableSorter.js
+++ src/main/resources/static/js/TableSorter.js
@@ -1,32 +1,20 @@
 $(document).ready(function() {
+	//hides approved items in table
 	$('.hideAppr').on('click', function () {
   		var $rowsAppr = $('#info-systems-table tbody tr').filter(function () {
             return $.trim($(this).find('td').eq(6).text()) !== ""}).toggle();
-  		
-  		//for normal button
-  		//$(this).text(function(i, v){
-          // return v === 'Näita kooskõlastamist vajavaid' ? 'Näita Kõiki' : 'Näita kooskõlastamist vajavaid'})
+  	
     });
-
+	
+	//hides not approved items in table 
   	$('.hideEmptyAppr').on('click', function () {
+  		
   		var $rowsEmptyAppr = $('#info-systems-table tbody tr').filter(function () {
             return $.trim($(this).find('td').eq(6).text()) === ""}).toggle();
-  	//for normal button
-  		//$(this).text(function(i, v){
-           //return v === 'Näita ainult kooskõlastatud' ? 'Näita Kõiki' : 'Näita ainult kooskõlastatud'})
-    });
-// old code for owner search bar with buttons
-//  	$('.findOwner').on('click', function () {
-//  		var $rowsOwner = $('#info-systems-table tbody tr').filter(function () {
-//  			return $.trim($(this).find('td').eq(0).text() !== document.getElementById("owner").value}).toggle();
-//  		//return $.trim($(this).find('td:not(:contains(("#ownerS").value))').eq(0).text())}).toggle();   //
-//  		$(this).text(function(i, v){
-//       		return v === 'Otsi omaniku' ? 'Näita Kõiki' : 'Otsi omaniku'})
-//  	}));
+  	});
   	
-
+  	//search bar function for individual columns
     $('.dataTable .filters input').keyup(function(e){
-
     	var $input = $(this);
         inputContent = $input.val().toLowerCase();
         tableDiv = $input.parents('.table-responsive');
@@ -48,64 +36,4 @@ $(document).ready(function() {
             $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">Tulemusi ei leitud.</td></tr>'));
         }
     });
-    
-    
-    $('.dataTable .filters select').on('change',function(e){
-    	var $input = $(this);
-        inputContent = $input.val().toLowerCase();
-        tableDiv = $input.parents('.table-responsive');
-        column = tableDiv.find('.filters th').index($input.parents('th'));
-        $table = tableDiv.find('.dataTable');
-        $rows = $table.find('tbody tr');
-        
-        var dt = new Date();
-        
-   		var rex = new RegExp($('#filterText').val());
-   		var value = $(this).find('td').eq(column).text().toLowerCase();
-   		var valStr = value.toString();
-   		var valDt = valStr.substring(0, 10);
-   		var valTm = valStr.substring(11, 16);
-   		var dtSplit = valDt.split("-");
-   		
-   		//2017-06-02T09:36:03.42
-   		
-   		var valYr = dtSplit(0);
-   		var valMn = dtSplit(1);
-   		var valDy = dtSplit(2);
-   		
-   		var valDate = new Date(valMn, valDy, valYr, valTm);
-   		
-   		var $filteredRows = $rows.filter(function(){
-            return value.indexOf(inputContent) === -1;
-        });
-   		
-   		if(rex =="/all/"){
-    		$rows.show()
-    		}
-    	else if(rex =="/7days/"){
-    			
-    		if (dt - (valDate+ 10,800,000) >= 604800000) {
-				$rows.show();
-			}else {
-				$rows.hide();
-			}
-    		
-    		
-    		return value.indexOf(inputContent) === -1;
-    	}
-    	else if(rex =="/1hour/"){
-			
-    		if (dt - (valDate+10,800,000) >= 3600000) {
-				$rows.show();
-			}else {
-				$rows.hide();
-			}
-    		
-    		
-    		return value.indexOf(inputContent) === -1;
-    	}
-   		
-       
-    });
-
 });
